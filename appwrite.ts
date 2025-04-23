2871a1755fce62c6bb55a959f699140d91e7ee16
malhardv <malhar.vhatkar@gmail.com>
2025-04-23 16:50:54 +0530

    m

--- appwrite.ts
+++ appwrite.ts
@@ -2,8 +2,7 @@ import { Account, Client, Databases, Storage, ID } from "appwrite";
 
 const client = new Client()
   .setEndpoint("https://fra.cloud.appwrite.io/v1")
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_ID!)
-  .setSelfSigned(true); // For self-signed certificates, only use during development
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_ID!);
 
 const account = new Account(client);
 const databases = new Databases(client);
